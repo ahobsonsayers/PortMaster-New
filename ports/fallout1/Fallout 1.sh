7deae0a6cfd2688523ef452806ad41f6842ee07a
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2024-12-24 21:57:14 +0000

    Fix Fallout launcher script

--- ports/fallout1/Fallout 1.sh
+++ ports/fallout1/Fallout 1.sh
@@ -19,12 +19,18 @@ source $controlfolder/device_info.txt
 PORTNAME="Fallout 1"
 
 to_lower_case() {
-    for SRC in $(find "$1" -depth); do
+  for SRC in $(find "$1" -depth); do
     DST=$(dirname "${SRC}")/$(basename "${SRC}" | tr '[A-Z]' '[a-z]')
+    TEMP_DST="$DST.temp"
     if [ "${SRC}" != "${DST}" ]; then
-        [ ! -e "${DST}" ] && $ESUDO mv -vT "${SRC}" "${DST}" || echo "- ${SRC} was not renamed"
+      if [ -e "${DST}" ]; then
+        echo "- ${DST} already exists"
+      else
+        $ESUDO mv -vT "${SRC}" "${TEMP_DST}" || echo "- ${SRC} was not renamed"
+        $ESUDO mv -vT "${TEMP_DST}" "${DST}" || echo "- ${SRC} was not renamed"
+      fi
     fi
-    done
+  done
 }
 
 get_controls
@@ -34,26 +40,26 @@ PORTDIR="/$directory/ports/fallout1"
 cd "$PORTDIR"
 
 for file in data critter.dat master.dat; do
-    if [[ ! -e "$file" ]]; then
-        file_uc=$(echo "$file" | tr '[a-z]' '[A-Z]')
+  if [[ ! -e "$file" ]]; then
+    file_uc=$(echo "$file" | tr '[a-z]' '[A-Z]')
 
-        if [[ -e "$file_uc" ]]; then
-            # File exists but it is in uppercase, make it lowercase.
-            to_lower_case "${PORTDIR}"
+    if [[ -e "$file_uc" ]]; then
+      # File exists but it is in uppercase, make it lowercase.
+      to_lower_case "${PORTDIR}"
 
-            if [[ ! -e "$file" ]]; then
-                echo "Missing file: $file" > /dev/tty1
-                sleep 5
-                printf "\033c" >> /dev/tty1
-                exit 1
-            fi
-        else
-            echo "Missing file: $file" > /dev/tty1
-            sleep 5
-            printf "\033c" >> /dev/tty1
-            exit 1
-        fi
+      if [[ ! -e "$file" ]]; then
+        echo "Missing file: $file" > /dev/tty1
+        sleep 5
+        printf "\033c" >> /dev/tty1
+        exit 1
+      fi
+    else
+      echo "Missing file: $file" > /dev/tty1
+      sleep 5
+      printf "\033c" >> /dev/tty1
+      exit 1
     fi
+  fi
 done
 
 $ESUDO chmod 666 /dev/uinput
@@ -65,13 +71,12 @@ export TEXTINPUTNOAUTOCAPITALS="Y"
 
 $GPTOKEYB "fallout-ce" -c "./fallout1.gptk.$ANALOG_STICKS" textinput &
 if [[ $whichos == *"ArkOS"* ]]; then
-    LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.10.0 ./fallout-ce 2>&1 | tee -a ./log.txt
+  LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.10.0 ./fallout-ce 2>&1 | tee -a ./log.txt
 else
-    ./fallout-ce 2>&1 | tee -a ./log.txt
+  ./fallout-ce 2>&1 | tee -a ./log.txt
 fi
 
 $ESUDO kill -9 $(pidof gptokeyb)
 unset SDL_GAMECONTROLLERCONFIG
 $ESUDO systemctl restart oga_events &
 printf "\033c" >> /dev/tty1
-
